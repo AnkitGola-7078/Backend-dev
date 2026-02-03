97b22d823cbe0d030c8f573bb2c6b2e7eb26a0ef
ANKIT GOLA <ankitprajapati3785@gmail.com>
2026-02-03 16:15:20 +0530

    3/feb/2026,today we learn about  Post/Put Apis

--- ExpressJs-Backend/index.js
+++ ExpressJs-Backend/index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 const students = [
     { id: 1, name: "Rahul", age: 20, class: "B.Tech" },
     { id: 2, name: "Priya", age: 21, class: "B.Sc" },
-    { id: 3, name: "Amit", age: 22, class: "MCA" }
+    { id: 3, name: "bhavya", age: 22, class: "MCA" }
 ];
 
 app.get('/', (req, res) => {
@@ -44,12 +44,29 @@ app.get('/branch/:branch', (req, res) => {
 app.post('/students/registers',(req,res)=>{
     const newStudent = req.body;
     console.log(newStudent);
-    if(!newStudent){
+    if(!newStudent || !newStudent.id || !newStudent.name || !newStudent.age || !newStudent.class){
         return res.status(404).send("Internal server error");
     }
+    //check if student with same id already exists
+    if(students.find((s)=>s.id === newStudent.id)){
+        return res.status(400).send("Student with this id already exists");
+    }
+
     students.push(newStudent);
     res.status(200).json(newStudent);
 })
+//---------------------------------------------------PUT-------------------------------------------------
+app.put('/students/update/:id',(req,res)=>{
+    const id = parseInt(req.params.id);
+    const updatedData = req.body;
+    const stuIdx=students.findIndex((s)=>s.id === id);
+    if(stuIdx === -1){
+        return res.status(400).send("Student not present");
+    }
+    console.log(stuIdx);
+    students[stuIdx] = {...students[stuIdx],...updatedData};
+    res.status(200).json(students[stuIdx]);
+});
 
 
 app.listen(PORT, () => {
